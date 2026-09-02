df802d59a2920a17b1ef3d26f91e978aac5f0f8a
amir vahid <50135057+vahid162@users.noreply.github.com>
2026-09-02 13:19:22 +0200

    feat: add integration/functions-loader.php

---
+++ integration/functions-loader.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Add this require_once line to woodmart-child/functions.php during staging.
+ *
+ * Do not replace the existing child-theme functions.php file with this file.
+ */
+
+require_once get_stylesheet_directory() . '/src/homepage/bootstrap.php';
